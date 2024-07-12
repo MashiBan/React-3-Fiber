d5d8746f741c3445d280bfdcd548d6d564a99d3b
MashiBan <kb00664422@gmail.com>
2024-07-12 13:53:41 +0530

    completed

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,18 +1,79 @@
+// import {  useThree, extend, useFrame } from "@react-three/fiber";
+// import { useRef } from "react";
+// import CustomObject from "./CustomObject.jsx";
+// // import { OrbitControls  } from "three/examples/jsm/controls/OrbitControls.js"; //OrbitControlls not working
+
+// // extend({ OrbitControls }); not working
+// export default function Experience()
+// {
+//     const cubeRef = useRef();
+//     const groupRef = useRef();
+//     const { camera, gl } = useThree();
+//     // console.log("camera", camera, " gl", gl);
+    
+   
+//     useFrame((state, delta) => {
+//         // console.log(delta);
+//         //we prefer to use delta because it allow same rotation speed on every device
+//         // groupRef.current.rotation.y += delta 
+//         cubeRef.current.rotation.y += delta 
+//     })
+
+
+
+//     return <>
+//     {/* <OrbitControls args={[camera, gl.domElement]} /> */}
+
+//     <directionalLight position={[ 1, 2, 3]} intensity={ 1.5 }/>
+//     <ambientLight intensity={ 1.5 }/>
+
+//         <group ref={ groupRef }>
+//             <mesh position-x={ -2.5 }>
+//                 {/* <sphereGeometry args={ [ 1.5, 32, 32 ] }/>  */}
+//                 {/* <meshBasicMaterial args={ [ { color:'blue', wireframe: true} ] }/> */}
+//                 {/* <meshBasicMaterial color="mediumpurple" wireframe={true}/> */}
+
+//                 <sphereGeometry args={ [1.2, 32 ,32 ] } />
+//                 <meshStandardMaterial color="orange"/>
+            
+//             </mesh>
+//             <mesh ref={ cubeRef } position-x={ 2.2 } scale={1.8} >
+//                 <boxGeometry />
+//                 <meshStandardMaterial color="mediumpurple"/>
+//             </mesh>
+//         </group>
+//             <mesh scale={ 10 } rotation={[-1.2,0, 0]} position={[0, -1.5, 0]}>
+//                 <planeGeometry rotateX={ 10 } />
+//                 <meshStandardMaterial color="greenyellow"/>
+//             </mesh>
+//         <CustomObject/>
+//     </>
+// }
+
 import {  useThree, extend, useFrame } from "@react-three/fiber";
 import { useRef } from "react";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import CustomObject from "./CustomObject.jsx";
+import * as THREE from "three";
+// import { OrbitControls  } from "three/examples/jsm/controls/OrbitControls.js"; //OrbitControlls not working
 
-extend({ orbitControls: OrbitControls });
+// extend({ OrbitControls }); not working
 export default function Experience()
 {
     const cubeRef = useRef();
-    const groupRef = useRef();
+    // const groupRef = useRef();
     const { camera, gl } = useThree();
-  console.log("camera", camera, " gl", gl);
+    // console.log("camera", camera, " gl", gl);
     
    
     useFrame((state, delta) => {
-        // console.log(delta);
+        // console.log(state.clock.elapsedTime);
+        // console.log(state.camera);
+
+        // const angle = state.clock.elapsedTime;
+        // state.camera.position.x = Math.sin(angle) * 8
+        // state.camera.position.z = Math.cos(angle) * 8
+        // state.camera.lookAt(0, 0, 0)
+
         //we prefer to use delta because it allow same rotation speed on every device
         // groupRef.current.rotation.y += delta 
         cubeRef.current.rotation.y += delta 
@@ -21,25 +82,31 @@ export default function Experience()
 
 
     return <>
-    <orbitControls args={[camera, gl.domElement]} />
-        <group ref={ groupRef }>
+    {/* <OrbitControls args={[camera, gl.domElement]} /> */}
+
+    <directionalLight position={[ 1, 2, 3]} intensity={ 1.5 }/>
+    <ambientLight intensity={ 1.5 }/>
+
+        {/* <group ref={ groupRef }> */}
+        <CustomObject/>
             <mesh position-x={ -2.5 }>
                 {/* <sphereGeometry args={ [ 1.5, 32, 32 ] }/>  */}
                 {/* <meshBasicMaterial args={ [ { color:'blue', wireframe: true} ] }/> */}
                 {/* <meshBasicMaterial color="mediumpurple" wireframe={true}/> */}
 
                 <sphereGeometry args={ [1.2, 32 ,32 ] } />
-                <meshBasicMaterial color="orange"/>
+                <meshStandardMaterial color="orange"/>
             
             </mesh>
-            <mesh ref={ cubeRef } position-x={ 2.2 } scale={1.8} >
+            <mesh ref={cubeRef} position-x={ 2.2 } scale={1.8} >
                 <boxGeometry />
-                <meshBasicMaterial color="mediumpurple"/>
+                <meshStandardMaterial color="mediumpurple"/>
             </mesh>
-        </group>
-            <mesh scale={ 10 } rotation={[-1.2,0, 0]} position={[0, -1.5, 0]}>
+        {/* </group> */}
+            <mesh scale={ 10 } rotation={[-Math.PI * 0.5, 0, 0]} position={[0, -1.5, 0]} >
                 <planeGeometry rotateX={ 10 } />
-                <meshBasicMaterial color="greenyellow"/>
+                <meshStandardMaterial color="greenyellow" side={THREE.DoubleSide}/>
             </mesh>
+        
     </>
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,30 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
+// import { CineonToneMapping } from 'three'
+import * as THREE from 'three'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
-
+const cameraSettigns = {
+    fov: 45, 
+    zoom: 100,
+    near: 0.1,
+    far: 200,
+    position: [3, 2, 6]
+}
 root.render(
     <>
-        <Canvas>
+        <Canvas
+            orthographic
+            // flat
+            gl={ {
+                antialias : true,
+                // toneMapping: THREE.CineonToneMapping
+                toneMapping: THREE.ACESFilmicToneMapping,
+                // outputColorSpace: THREE.SRGBColorSpace
+            }}
+            camera={ cameraSettigns }
+        >
             <Experience/>
         </Canvas>
     </>
